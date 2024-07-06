d38218245161528b732126e4ff6e8f177a56ba7b
yslaoui <yslaouiyassine@gmail.com>
2024-07-06 14:58:26 +0100

    Adding GUI of chart

--- LargeNumbers.java
+++ LargeNumbers.java
@@ -1,5 +1,36 @@
 package largeNumbers;
 
-public class LargeNumbers {
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 
+public class LargeNumbers extends Application {
+    @Override
+    public void start(Stage window) throws Exception {
+
+        NumberAxis xAxis = new NumberAxis(0, 100, 10);
+        NumberAxis yAxis = new NumberAxis(1,6,1);
+
+        LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        ObservableList<XYChart.Data<Number, Number>> data = series.getData();
+
+
+
+        chart.getData().add(series);
+
+        Scene scene = new Scene(chart);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(LargeNumbers.class);
+    }
 }
